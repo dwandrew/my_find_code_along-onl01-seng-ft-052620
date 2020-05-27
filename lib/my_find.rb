a26eb4ea5939e38ea248bbd9639a7016308b1b3a
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 10:39:07 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,9 @@ def my_find(collection)
  if block_given?
    i=0;
    while i<collection.length;
-   yield(collection[i])
+   if yield(collection[i]) == true;
+     return collection[i]
+    end
    i+=1
     end
   else "No block given"
